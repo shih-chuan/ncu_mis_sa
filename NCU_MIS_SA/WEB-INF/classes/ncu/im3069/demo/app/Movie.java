70fc1316b22f0abe8fc78709f0193c1487467ca2
Emily <emily938077@gmail.com>
2020-12-15 23:28:12 +0800

    add Movie MovieController MovieHelper Session SessionHelper

---
+++ NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/app/Movie.java
@@ -0,0 +1,155 @@
+package ncu.im3069.demo.app;
+
+import org.json.*;
+import java.util.*;
+
+public class Movie {
+
+    /** id，電影編號 */
+    private int id;
+
+    /** name，電影名稱 */
+    private String name;
+
+    /** cover，電影海報 */
+    private String cover;
+
+    /** content，電影描述 */
+	private String content;
+	
+	/** running_time，電影時長 */
+	private int running_time;
+	
+    /** genre，電影類別 */
+	private String genre;	
+	
+    /** release_date，電影上映日期 */
+	private Date release_date;
+	
+
+    /**
+     * 實例化（Instantiates）一個新的（new）Product 物件<br>
+     * 採用多載（overload）方法進行，此建構子用於新增產品時
+     *
+     * @param id 產品編號
+     */
+	public Movie(int id) {
+		this.id = id;
+	}
+
+    /**
+     * 實例化（Instantiates）一個新的（new）Product 物件<br>
+     * 採用多載（overload）方法進行，此建構子用於新增產品時
+     *
+     * @param name 產品名稱
+     * @param price 產品價格
+     * @param image 產品圖片
+     */
+	/*public Movie(String name, double price, String image) {
+		this.name = name;
+		this.price = price;
+		this.image = image;
+	}
+	*/
+    /**
+     * 實例化（Instantiates）一個新的（new）Product 物件<br>
+     * 採用多載（overload）方法進行，此建構子用於修改產品時
+     *
+     * @param id 產品編號
+     * @param name 產品名稱
+     * @param price 產品價格
+     * @param image 產品圖片
+     * @param describe 產品敘述
+     */
+	public Movie(int id, String name, String cover, String content, 
+			int running_time, String genre, Date release_date) {
+		this.id = id;
+		this.name = name;
+		this.cover = cover;
+		this.content = content;
+		this.running_time = running_time;
+		this.genre = genre;
+		this.release_date = release_date;
+	}
+
+    /**
+     * 取得產品編號
+     *
+     * @return int 回傳電影編號
+     */
+	public int getID() {
+		return this.id;
+	}
+
+    /**
+     * 取得產品名稱
+     *
+     * @return String 回傳電影名稱
+     */
+	public String getName() {
+		return this.name;
+	}
+	
+	 /**
+     * 取得產品名稱
+     *
+     * @return String 回傳電影海報
+     */
+	public String getCover() {
+		return this.cover;
+	}
+    /**
+     * 取得電影描述
+     *
+     * @return String 回傳電影描述
+     */
+	public String getContent() {
+		return this.content;
+	}
+
+    /**
+     * 取得產品圖片
+     *
+     * @return  回傳電影時常
+     */
+	public int getRunning_time() {
+		return this.running_time;
+	}
+
+    /**
+     * 取得電影種類
+     *
+     * @return String 回傳電影種類
+     */
+	public String getGenre() {
+		return this.genre;
+	}
+	
+	 /**
+     * 取得電影種類
+     *
+     * @return String 回傳電影種類
+     */
+	public Date getRelease_date() {
+		return this.release_date;
+	}
+	
+    /**
+     * 取得產品資訊
+     *
+     * @return JSONObject 回傳產品資訊
+     */
+	public JSONObject getData() {
+        /** 透過JSONObject將該項產品所需之資料全部進行封裝*/
+        JSONObject jso = new JSONObject();
+        jso.put("id", getID());
+        jso.put("name", getName());
+        jso.put("cover", getCover());
+        jso.put("content", getContent());
+        jso.put("running_time", getRunning_time());
+        jso.put("genre", getGenre());
+        jso.put("release_date", getRelease_date());
+        
+        return jso;
+    }
+}
---
+++ NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/app/Session.java
@@ -0,0 +1,136 @@
+package ncu.im3069.demo.app;
+
+import org.json.*;
+import java.util.*;
+
+public class Session{
+
+    /** session_id，場次編號 */
+    private int session_id;
+    
+    private Movie movie;
+    
+    private Theater theater;
+    
+    /** session_time，場次時間 */
+	private Date session_time;	
+	
+    /** session_date，場次日期 */
+	private Date session_date;
+	
+    /** th，ProductHelper 之物件與 OrderItem 相關之資料庫方法（Sigleton） */
+    private TheaterHelper th =  TheaterHelper.getHelper();
+    
+    /** th，ProductHelper 之物件與 OrderItem 相關之資料庫方法（Sigleton） */
+    private MovieHelper mh =  MovieHelper.getHelper();
+    
+    /**
+     * 實例化（Instantiates）一個新的（new）Product 物件<br>
+     * 採用多載（overload）方法進行，此建構子用於新增產品時
+     *
+     * @param id 產品編號
+     */
+	public Session(int session_id) {
+		this.session_id = session_id;
+	}
+
+    /**
+     * 實例化（Instantiates）一個新的（new）Product 物件<br>
+     * 採用多載（overload）方法進行，此建構子用於新增產品時
+     *
+     * @param name 產品名稱
+     * @param price 產品價格
+     * @param image 產品圖片
+     */
+	public Session(int session_id, Date session_time, Date session_date) {
+		this.session_id = session_id;
+		this.session_time = session_time;
+		this.session_date = session_date;
+	}
+	
+    /**
+     * 實例化（Instantiates）一個新的（new）Product 物件<br>
+     * 採用多載（overload）方法進行，此建構子用於修改產品時
+     *
+     * @param id 產品編號
+     * @param name 產品名稱
+     * @param price 產品價格
+     * @param image 產品圖片
+     * @param describe 產品敘述
+     */
+	public Session(int session_id, int movie_id, int theater_id, Date session_time
+			, Date session_date) {
+		this.session_id = session_id;
+		getTheaterFromDB(theater_id);
+		getMovieFromDB(movie_id);
+		this.session_time = session_time;
+		this.session_date = session_date;
+	}
+    
+    /**
+     * 取得場次編號
+     *
+     * @return int 回傳場次編號
+     */
+	public int getSession_id() {
+		return this.session_id;
+	}
+
+	public Theater getTheater() {
+		return this.theater;
+	}
+
+	public Movie getMovie() {
+		return this.movie;
+	}
+
+    /**
+     * 取得場次時間
+     *
+     * @return String 回傳場次時間
+     */
+	public Date getSession_time() {
+		return this.session_time;
+	}
+
+    /**
+     * 取得場次日期
+     *
+     * @return  回傳場次日期
+     */
+	public Date getSession_date() {
+		return this.session_date;
+	}
+
+	/**
+     * 從 DB 中取得影廳
+     */
+    private void getTheaterFromDB(int theater_id) {
+        String tid = String.valueOf(theater_id);
+        this.theater = th.getById(tid);
+    }
+    
+	/**
+     * 從 DB 中取得影廳
+     */
+    private void getMovieFromDB(int movie_id) {
+        String mid = String.valueOf(movie_id);
+        this.movie = mh.getById(mid);
+    }
+        
+    /**
+     * 取得產品資訊
+     *
+     * @return JSONObject 回傳產品資訊
+     */
+	public JSONObject getData() {
+        /** 透過JSONObject將該項產品所需之資料全部進行封裝*/
+        JSONObject jso = new JSONObject();
+        jso.put("session_id", getSession_id());
+        jso.put("session_time", getSession_time());
+        jso.put("session_date", getSession_date());
+        
+        return jso;
+    }
+}
+
