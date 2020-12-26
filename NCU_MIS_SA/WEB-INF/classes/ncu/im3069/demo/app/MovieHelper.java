2f3a2ea854045f30e9c7adbf7f8b710e1de4d1a3
Emily <emily938077@gmail.com>
2020-12-27 00:02:12 +0800

    增加 新增刪除修改場次  修改場次為簡易版

--- NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/app/MovieHelper.java
+++ NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/app/MovieHelper.java
@@ -45,7 +45,7 @@ public class MovieHelper {
             /** 取得資料庫之連線 */
             conn = DBMgr.getConnection();
             /** SQL指令 */
-            String sql = "SELECT * FROM `missa`.`movie`";
+            String sql = "SELECT * FROM `missa`.`movies`";
             
             /** 將參數回填至SQL指令當中，若無則不用只需要執行 prepareStatement */
             pres = conn.prepareStatement(sql);
@@ -121,7 +121,7 @@ public class MovieHelper {
           conn = DBMgr.getConnection();
           String[] in_para = DBMgr.stringToArray(data, ",");
           /** SQL指令 */
-          String sql = "SELECT * FROM `missa`.`movie` WHERE `movie`.`movie_id`";
+          String sql = "SELECT * FROM `missa`.`movies` WHERE `movies`.`movie_id`";
           for (int i=0 ; i < in_para.length ; i++) {
               sql += (i == 0) ? "in (?" : ", ?";
               sql += (i == in_para.length-1) ? ")" : "";
@@ -185,7 +185,7 @@ public class MovieHelper {
       return response;
   }
     
-    public JSONObject getMovieById(String id) {
+    public JSONObject getById(String id) {
         /** 新建一個 Product 物件之 m 變數，用於紀錄每一位查詢回之商品資料 */
     	Movie m = null;
     	/** 用於儲存所有檢索回之會員，以JSONArray方式儲存 */
@@ -203,7 +203,7 @@ public class MovieHelper {
             /** 取得資料庫之連線 */
             conn = DBMgr.getConnection();
             /** SQL指令 */
-            String sql = "SELECT * FROM `missa`.`movie` WHERE `movie_id` = ? LIMIT 1";
+            String sql = "SELECT * FROM `missa`.`movies` WHERE `movie_id` = ? LIMIT 1";
             
             /** 將參數回填至SQL指令當中，若無則不用只需要執行 prepareStatement */
             pres = conn.prepareStatement(sql);
@@ -264,6 +264,74 @@ public class MovieHelper {
 
     }
     
+    public Movie getMovieById(String id) {
+        /** 新建一個 Product 物件之 m 變數，用於紀錄每一位查詢回之商品資料 */
+    	Movie m = null;
+    	/** 用於儲存所有檢索回之會員，以JSONArray方式儲存 */
+        JSONArray jsa = new JSONArray();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
+        
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "SELECT * FROM `missa`.`movies` WHERE `movie_id` = ? LIMIT 1";
+            
+            /** 將參數回填至SQL指令當中，若無則不用只需要執行 prepareStatement */
+            pres = conn.prepareStatement(sql);
+            pres.setString(1, id);
+            /** 執行查詢之SQL指令並記錄其回傳之資料 */
+            rs = pres.executeQuery();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+            
+            /** 透過 while 迴圈移動pointer，取得每一筆回傳資料 */
+            while(rs.next()) {
+            	/** 每執行一次迴圈表示有一筆資料 */
+            	row+=1;
+                /** 將 ResultSet 之資料取出 */
+            	int movie_id = rs.getInt("movie_id");
+                String movie_name = rs.getString("movie_name");
+                System.out.print(movie_name);
+                String cover = rs.getString("movie_cover");
+                String content = rs.getString("movie_content");
+                System.out.print(content);
+
+                int running_time = rs.getInt("running_time");
+                String genre = rs.getString("genre");
+                Date release_date = rs.getDate("release_date");
+                
+                
+                /** 將每一筆商品資料產生一名新Product物件 */
+                m = new Movie(movie_id, movie_name, cover, content, running_time, genre, release_date);
+                /** 取出該名會員之資料並封裝至 JSONsonArray 內 */
+                jsa.put(m.getData());
+            }
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
+        }
+        
+        return m;
+
+    }
+    
     
     public JSONObject deleteByID(int id) {
         /** 記錄實際執行之SQL指令 */
@@ -280,7 +348,7 @@ public class MovieHelper {
             conn = DBMgr.getConnection();
             
             /** SQL指令 */
-            String sql = "DELETE FROM `missa`.`movie` WHERE `movie_id` = ? LIMIT 1";
+            String sql = "DELETE FROM `missa`.`movies` WHERE `movie_id` = ? LIMIT 1";
             
             /** 將參數回填至SQL指令當中 */
             pres = conn.prepareStatement(sql);
@@ -337,7 +405,7 @@ public class MovieHelper {
             /** 取得資料庫之連線 */
             conn = DBMgr.getConnection();
             /** SQL指令 */
-            String sql = "Update `missa`.`movie` SET `movie_name` = ? ,`movie_cover` = ? , "
+            String sql = "Update `missa`.`movies` SET `movie_name` = ? ,`movie_cover` = ? , "
             		+ "`movie_content` = ? , `running_time` = ? , `genre` = ? , `release_date` = ? WHERE `movie_id` = ?";
             /** 取得所需之參數 */
             int movie_id = m.getID();
@@ -404,7 +472,7 @@ public class MovieHelper {
             /** 取得資料庫之連線 */
             conn = DBMgr.getConnection();
             /** SQL指令 */
-            String sql = "INSERT INTO `missa`.`movie`(`movie_name`, `movie_cover`, `movie_content`, `running_time`, `genre`, `release_date`)"
+            String sql = "INSERT INTO `missa`.`movies`(`movie_name`, `movie_cover`, `movie_content`, `running_time`, `genre`, `release_date`)"
                     + " VALUES(?, ?, ?, ?, ?, ?)";
             /** 取得所需之參數 */
             String movie_name = m.getName();
--- NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/app/SessionHelper.java
+++ NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/app/SessionHelper.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 import org.json.JSONArray;
@@ -45,8 +46,8 @@ public class SessionHelper {
             
             /** 取得所需之參數 */
             int session_id = session.getId();
-            Date session_time = session.getSession_time();
-            Date session_date = session.getSession_date();
+            String session_time = session.getSession_time();
+            String session_date = session.getSession_date();
             
             try {
                 /** 取得資料庫之連線 */
@@ -60,8 +61,8 @@ public class SessionHelper {
                 pres.setInt(1, session_id);
                 pres.setInt(2, theater_id);
                 pres.setInt(3, movie_id);
-                pres.setDate(4, (java.sql.Date) session_time);
-                pres.setDate(5, (java.sql.Date) session_date);
+                pres.setString(4,session_time);
+                pres.setString(5,session_date);
                 
                 /** 執行新增之SQL指令並記錄影響之行數 */
                 pres.executeUpdate();
@@ -91,11 +92,14 @@ public class SessionHelper {
         
         return jsa;
     }
-    
+    /** 用於bookseat*/
     public JSONObject getById(String id) {
         JSONObject data = new JSONObject();
         /** 新建一個 Session 物件之 s 變數，用於紀錄每一位查詢回之商品資料 */
     	Session s = null;
+    	
+        JSONArray jsa = new JSONArray();
+
         /** 記錄實際執行之SQL指令 */
         String exexcute_sql = "";
         /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
@@ -129,7 +133,8 @@ public class SessionHelper {
                 /** 將每一筆商品資料產生一名新Product物件 */
                 s = new Session(session_id, movie_id, theater_id,session_date, session_time);
                 /** 取出該項商品之資料並封裝至 JSONsonArray 內 */
-                data = s.getSessionAllInfo();
+                //data = s.getSessionAllInfo();
+                data = s.getData();
             }
 
         } catch (SQLException e) {
@@ -142,7 +147,7 @@ public class SessionHelper {
             /** 關閉連線並釋放所有資料庫相關之資源 **/
             DBMgr.close(rs, pres, conn);
         }
-
+        
         return data;
     }
 
@@ -197,43 +202,105 @@ public class SessionHelper {
         return s;
     }
     
-    public ArrayList<Session> getSessionsByMovieIdandTheaterId(int movie_id, int theater_id) {
-        ArrayList<Session> result = new ArrayList<Session>();
+    public JSONObject create(Session s) {
         /** 記錄實際執行之SQL指令 */
         String exexcute_sql = "";
-        ResultSet rs = null;
-        Session session = null;
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
         
         try {
             /** 取得資料庫之連線 */
             conn = DBMgr.getConnection();
             /** SQL指令 */
-            String sql = "SELECT * FROM `missa`.`session` WHERE `session`.`movie_id` = ?";
+            String sql = "INSERT INTO `missa`.`session`(`movie_id`, `theater_id`, `session_date`, `session_time`)"
+                    + " VALUES( ?, ?, ?, ?)";
+            /** 取得所需之參數 */
+            int movie_id = s.getMovie_id();
+            int theater_id = s.getTheater_id();
+            String session_date = s.getSession_date();
+            String session_time = s.getSession_time();
             
             /** 將參數回填至SQL指令當中 */
             pres = conn.prepareStatement(sql);
             pres.setInt(1, movie_id);
+            pres.setInt(2, theater_id);
+            pres.setString(3, session_date);
+            pres.setString(4, session_time);
             
-            /** 執行新增之SQL指令並記錄影響之行數 */
-            rs = pres.executeQuery();
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+            /** 執行更新之SQL指令並記錄影響之行數 */
+            row = pres.executeUpdate();
             
             /** 紀錄真實執行的SQL指令，並印出 **/
             exexcute_sql = pres.toString();
             System.out.println(exexcute_sql);
+
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(pres, conn);
+        }
+
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+
+        /** 將SQL指令、花費時間與影響行數，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("time", duration);
+        response.put("row", row);
+
+        return response;
+    }
+    
+    public JSONObject update(Session s) {
+        /** 紀錄回傳之資料 */
+        JSONArray jsa = new JSONArray();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "Update `missa`.`session` SET `movie_id` = ? ,`theater_id` = ? , "
+            		+ "`session_date` = ? , `session_time` = ? WHERE `session_id` = ?";
+            /** 取得所需之參數 */
+            int session_id = s.getId();
+            int movie_id = s.getMovie_id();
+            int theater_id = s.getTheater_id();
+            String session_date = s.getSession_date();
+            String session_time = s.getSession_time();
             
-            while(rs.next()) {
-                /** 每執行一次迴圈表示有一筆資料 */
-                
-                /** 將 ResultSet 之資料取出 */
-                int session_id = session.getSession_id();
-                Date session_time = session.getSession_time();
-                Date session_date = session.getSession_date();
-                
-                /** 將每一筆會員資料產生一名新Member物件 */
-                session = new Session(session_id, movie_id, theater_id, session_time, session_date);
-                /** 取出該名會員之資料並封裝至 JSONsonArray 內 */
-                result.add(session);
-            }
+            
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setInt(1, movie_id);
+            pres.setInt(2, theater_id);
+            pres.setString(3, session_date);
+            pres.setString(4, session_time);
+            pres.setInt(5, session_id);
+            /** 執行更新之SQL指令並記錄影響之行數 */
+            row = pres.executeUpdate();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+
         } catch (SQLException e) {
             /** 印出JDBC SQL指令錯誤 **/
             System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
@@ -245,7 +312,143 @@ public class SessionHelper {
             DBMgr.close(pres, conn);
         }
         
-        return result;
-    }
-}
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+        
+        /** 將SQL指令、花費時間與影響行數，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+        response.put("data", jsa);
 
+        return response;
+    }
+    
+    public JSONObject getAll() {
+        /** 新建一個 Movie 物件之 m 變數，用於紀錄每一位查詢回之商品資料 */
+    	Session s = null;
+        /** 用於儲存所有檢索回之商品，以JSONArray方式儲存 */
+        JSONArray jsa = new JSONArray();
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
+        
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            /** SQL指令 */
+            String sql = "SELECT * FROM `missa`.`session`";
+            
+            /** 將參數回填至SQL指令當中，若無則不用只需要執行 prepareStatement */
+            pres = conn.prepareStatement(sql);
+            /** 執行查詢之SQL指令並記錄其回傳之資料 */
+            rs = pres.executeQuery();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+            
+            /** 透過 while 迴圈移動pointer，取得每一筆回傳資料 */
+            while(rs.next()) {
+                /** 每執行一次迴圈表示有一筆資料 */
+                row += 1;
+                /** 將 ResultSet 之資料取出 */
+                int session_id = rs.getInt("session_id");
+                int movie_id = rs.getInt("movie_id");
+                int theater_id = rs.getInt("theater_id");
+                String session_date = rs.getString("session_date");
+                String session_time = rs.getString("session_time");
+                String p = "p";
+                /** 將每一筆商品資料產生一名新Session物件 */		//跟老大用不同的session
+                s = new Session(session_id, movie_id, theater_id,session_date, session_time,p);	
+                /** 取出該項商品之資料並封裝至 JSONsonArray 內 */
+                jsa.put(s.getData());
+            }
+            
+            
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
+        }
+        
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+        
+        /** 將SQL指令、花費時間、影響行數與所有會員資料之JSONArray，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+        response.put("data", jsa);
+
+        return response;
+    }
+    public JSONObject deleteByID(int id) {
+        /** 記錄實際執行之SQL指令 */
+        String exexcute_sql = "";
+        /** 紀錄程式開始執行時間 */
+        long start_time = System.nanoTime();
+        /** 紀錄SQL總行數 */
+        int row = 0;
+        /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+        ResultSet rs = null;
+        
+        try {
+            /** 取得資料庫之連線 */
+            conn = DBMgr.getConnection();
+            
+            /** SQL指令 */
+            String sql = "DELETE FROM `missa`.`session` WHERE `session_id` = ? LIMIT 1";
+            
+            /** 將參數回填至SQL指令當中 */
+            pres = conn.prepareStatement(sql);
+            pres.setInt(1, id);
+            /** 執行刪除之SQL指令並記錄影響之行數 */
+            row = pres.executeUpdate();
+
+            /** 紀錄真實執行的SQL指令，並印出 **/
+            exexcute_sql = pres.toString();
+            System.out.println(exexcute_sql);
+            
+        } catch (SQLException e) {
+            /** 印出JDBC SQL指令錯誤 **/
+            System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            /** 若錯誤則印出錯誤訊息 */
+            e.printStackTrace();
+        } finally {
+            /** 關閉連線並釋放所有資料庫相關之資源 **/
+            DBMgr.close(rs, pres, conn);
+        }
+
+        /** 紀錄程式結束執行時間 */
+        long end_time = System.nanoTime();
+        /** 紀錄程式執行時間 */
+        long duration = (end_time - start_time);
+        
+        /** 將SQL指令、花費時間與影響行數，封裝成JSONObject回傳 */
+        JSONObject response = new JSONObject();
+        response.put("sql", exexcute_sql);
+        response.put("row", row);
+        response.put("time", duration);
+
+        return response;
+    }
+    
+}
