5d3b2b05a8f147079e559a061651c5796402eeb8
Emily <emily938077@gmail.com>
2020-12-29 13:27:32 +0800

    movie_detail time and date

--- NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/app/SessionHelper.java
+++ NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/app/SessionHelper.java
@@ -509,4 +509,78 @@ public class SessionHelper {
         return response;
     }
     
+
+    public JSONObject getByMovieId(String mid) {
+            JSONObject data = new JSONObject();
+            /** 新建一個 Session 物件之 s 變數，用於紀錄每一位查詢回之商品資料 */
+        	Session s = null;
+        	
+            JSONArray jsa = new JSONArray();
+
+            /** 記錄實際執行之SQL指令 */
+            String exexcute_sql = "";
+            
+            long start_time = System.nanoTime();
+            /** 紀錄SQL總行數 */
+            int row = 0;
+            /** 儲存JDBC檢索資料庫後回傳之結果，以 pointer 方式移動到下一筆資料 */
+            ResultSet rs = null;
+            
+            try {
+                /** 取得資料庫之連線 */
+                conn = DBMgr.getConnection();
+                /** SQL指令 */
+                String sql = "SELECT * FROM `missa`.`session` WHERE `session`.`movie_id` = ? ";
+                
+                /** 將參數回填至SQL指令當中，若無則不用只需要執行 prepareStatement */
+                pres = conn.prepareStatement(sql);
+                pres.setString(1, mid);
+                /** 執行查詢之SQL指令並記錄其回傳之資料 */
+                rs = pres.executeQuery();
+
+                /** 紀錄真實執行的SQL指令，並印出 **/
+                exexcute_sql = pres.toString();
+                System.out.println(exexcute_sql);
+                
+                /** 透過 while 迴圈移動pointer，取得每一筆回傳資料 */
+                while(rs.next()) {
+                    /** 將 ResultSet 之資料取出 */
+                	String p ="test";
+                	int session_id = rs.getInt("session_id");
+                    int movie_id = Integer.parseInt(mid);
+                    int theater_id = rs.getInt("theater_id");
+                    String session_date = rs.getString("session_date");
+                    String session_time = rs.getString("session_time");
+                    
+                    /** 將每一筆商品資料產生一名新Product物件 */
+                    s = new Session(session_id, movie_id, theater_id,session_time, session_date,p);
+                    /** 取出該項商品之資料並封裝至 JSONsonArray 內 */
+                    //data = s.getSessionAllInfo();
+                    jsa.put(s.getData());
+                }
+
+            } catch (SQLException e) {
+                /** 印出JDBC SQL指令錯誤 **/
+                System.err.format("SQL State: %s\n%s\n%s", e.getErrorCode(), e.getSQLState(), e.getMessage());
+            } catch (Exception e) {
+                /** 若錯誤則印出錯誤訊息 */
+                e.printStackTrace();
+            } finally {
+                /** 關閉連線並釋放所有資料庫相關之資源 **/
+                DBMgr.close(rs, pres, conn);
+            }
+            
+            long end_time = System.nanoTime();
+            long duration = (end_time - start_time);
+            
+            JSONObject response = new JSONObject();
+            response.put("sql", exexcute_sql);
+            response.put("row", row);
+            response.put("time", duration);
+            response.put("data", jsa);
+
+            return response;
+        }
+
+
 }
