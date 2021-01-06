22e12675a31edb8b6b2e2df4e6b92dc3eba06bbe
shih-chuan <shihchuan.4all@gmail.com>
2021-01-06 15:33:01 +0800

    delete MovieSessionController and Product

--- NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/controller/SessionController.java
+++ NCU_MIS_SA/WEB-INF/classes/ncu/im3069/demo/controller/SessionController.java
@@ -38,17 +38,20 @@ public class SessionController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         /** 透過JsonReader類別將Request之JSON格式資料解析並取回 */
         JsonReader jsr = new JsonReader(request);
+        JSONObject query = new JSONObject();
+        JSONObject resp = new JSONObject();
         /** 若直接透過前端AJAX之data以key=value之字串方式進行傳遞參數，可以直接由此方法取回資料 */
         String id = jsr.getParameter("id");
+        String by = jsr.getParameter("by");
         System.out.print("id=");
         System.out.println(id);
         /** 判斷該字串是否存在，若存在代表要取回個別會員之資料，否則代表要取回全部資料庫內會員之資料 */
         if (id.isEmpty()) {
             /** 透過MemberHelper物件之getAll()方法取回所有會員之資料，回傳之資料為JSONObject物件 */
-            JSONObject query = sh.getAll();
+            query = sh.getAll();
             
             /** 新建一個JSONObject用於將回傳之資料進行封裝 */
-            JSONObject resp = new JSONObject();
+            resp = new JSONObject();
             resp.put("status", "200");
             resp.put("message", "所有會員資料取得成功");
             resp.put("response", query);
@@ -57,17 +60,34 @@ public class SessionController extends HttpServlet {
             jsr.response(resp, response);
         }
         else {
-            /** 透過MemberHelper物件的getByID()方法自資料庫取回該名會員之資料，回傳之資料為JSONObject物件 */
-        	JSONObject query = sh.getAllInfoById(id);
+        	switch(by) {
+        		case "session":
+                    /** 透過MemberHelper物件的getByID()方法自資料庫取回該名會員之資料，回傳之資料為JSONObject物件 */
+                	query = sh.getAllInfoById(id);
+                    
+                    /** 新建一個JSONObject用於將回傳之資料進行封裝 */
+                    resp = new JSONObject();
+                    resp.put("status", "200");
+                    resp.put("message", "場次資料取得成功");
+                    resp.put("response", query);
             
-            /** 新建一個JSONObject用於將回傳之資料進行封裝 */
-            JSONObject resp = new JSONObject();
-            resp.put("status", "200");
-            resp.put("message", "場次資料取得成功");
-            resp.put("response", query);
-    
-            /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
-            jsr.response(resp, response);
+                    /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+                    jsr.response(resp, response);
+                    break;
+        		case "movie":
+                    /** 透過MemberHelper物件的getByID()方法自資料庫取回該名會員之資料，回傳之資料為JSONObject物件 */
+                    query = sh.getByMovieId(id);
+                    
+                     /**新建一個JSONObject用於將回傳之資料進行封裝 */
+                    resp = new JSONObject();
+                    resp.put("status", "200");
+                    resp.put("message", "電影資料取得成功");
+                    resp.put("response", query);
+            
+                    /** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+                    jsr.response(resp, response);
+                    break;
+        	}
         }
 	}
 
